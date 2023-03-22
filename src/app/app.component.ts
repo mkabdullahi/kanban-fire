976371b40993acac6e342cb9646d8dc645016b2c
habibu <habib.az@hotmail.com>
2023-03-22 21:25:44 +0100

    add: todos in the cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task/task.interface';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kanban-fire';
+ todos: Task[] = [
+    {
+      title: 'Buy Milk',
+      description: 'Go to the store and buy milk'
+    },
+    {
+      title: 'Create a Kanban app',
+      description: 'Using Firebase and Angular framework'
+    },
+    {
+      title: 'Add authentication',
+      description: 'Use firebase auth'
+    }
+ ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const MATERIAL_IMPORT = [
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ...MATERIAL_IMPORT
   ],
   providers: [],
   bootstrap: [AppComponent]
